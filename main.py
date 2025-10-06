6844736ca8b245fad13bc505559467c9c78706a7
Rahul6700 <rahulsk4087@gmail.com>
2025-10-06 15:18:05 +0530

    feat: target var grouping

--- main.py
+++ main.py
@@ -22,6 +22,24 @@ col_names = ['duration','protocol_type','service','flag','src_bytes','dst_bytes'
 test_df = pd.read_csv(test_file, header=None, names = col_names)
 train_df = pd.read_csv(train_file, header=None, names = col_names)
 
+#implement target variable grouping
+# need to reduce the many different outputs to 5 major ones
+# using a hashmap for O(1) lookups
+# Each specific attack type is a key and the values are their corresponding high level type
+attack_map = {
+    # DoS mappings
+    'back':'DoS', 'land':'DoS', 'neptune':'DoS', 'pod':'DoS', 'smurf':'DoS', 'teardrop':'DoS',
+    'apache2':'DoS', 'udpstorm':'DoS', 'processtable':'DoS', 'worm':'DoS',
+    # Probe
+    'satan':'Probe', 'ipsweep':'Probe', 'nmap':'Probe', 'portsweep':'Probe', 'mscan':'Probe', 'saint':'Probe',
+    # R2L
+    'ftp_write':'R2L', 'guess_passwd':'R2L', 'imap':'R2L', 'phf':'R2L', 'multihop':'R2L', 'warezmaster':'R2L',
+    'warezclient':'R2L', 'spy':'R2L', 'xlock':'R2L', 'xsnoop':'R2L', 'snmpguess':'R2L', 'snmpgetattack':'R2L',
+    'httptunnel':'R2L', 'sendmail':'R2L', 'named':'R2L',
+    # U2R
+    'buffer_overflow':'U2R', 'loadmodule':'U2R', 'rootkit':'U2R', 'perl':'U2R', 'sqlattack':'U2R',
+    'xterm':'U2R', 'ps':'U2R'
+}
 
 print(train_df.shape())
 print(train_df.head())
